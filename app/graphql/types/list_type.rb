53d90d9bc0f28b0027adafca4c18637d0107f73e
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-12-04 18:56:43 -0500

    Add todos field ot ListType

--- app/graphql/types/list_type.rb
+++ app/graphql/types/list_type.rb
@@ -4,5 +4,6 @@ module Types
 
     field :id, ID, 'The unique ID of this list', null: false
     field :name, String, 'The name of this list', null: false
+    field :todos, [TodoType], 'A list of todos associated with the list', null: false
   end
 end
